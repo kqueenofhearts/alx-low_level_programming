7bc8e6090b2df0c309c88a5de8f89c54683f80ce
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-08 16:30:49 -0700

    Basics

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 /**
- *
  *  * main - prints the size char,int,lomg int,long long int and float
- *
  *   * Return: 0 if success
- *
  **/
 
 int main(void)
@@ -23,6 +20,6 @@ int main(void)
 		printf("Size of an int: %d byte(s)\n", sizeof(b));
 		printf("Size of a long int: %d byte(s)\n", sizeof(c));
 		printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-		return 0;
+		return (0);
 
 }

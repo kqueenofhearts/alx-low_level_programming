1402b6d548d36efaa2859942b037a0309ad64922
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-08 16:41:56 -0700

    Basics

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
 		float e;
 
 		printf("Size of a char: %d byte(s)\n", sizeof(a));
-		printf("Size of a float: %d byte(s)\n", sizeof(e));
 		printf("Size of an int: %d byte(s)\n", sizeof(b));
 		printf("Size of a long int: %d byte(s)\n", sizeof(c));
 		printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+		printf("size of float: % bytes(s)\n", sizeof(e));
 		return (0);
 
 }

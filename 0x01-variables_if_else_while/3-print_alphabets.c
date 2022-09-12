26ae4f7d9b7ab1bed21c00b05c9f6841028d735f
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-12 08:23:09 -0700

    variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - Accept empty value
- *
- *   * putchar - Screen information
- *
- *    * @void: Description of a empty parameter
- *
- *     *
- *
- *      * Description: This is going print the alphabet in lower case
- *
- *       * Return: 0 Value if it works
- *
- *        */
+ * main - Accept empty value
+ * putchar - Screen information
+ * @void: Description of a empty parameter
+ * Description: This is going print the alphabet in lower case
+ * Return: 0 Value if it works
+ */
 
 int main(void)
 
 {
-{
-{	char x = 'a';
-{
-{	while (x <= 'z')
-{
-{	{
-{
-{		putchar(x);
-{
-{		x++;
-{
-{	}
-{
-{	putchar('\n');
-{
-{	return (0);
-{
-{}
+	char x = 'a';
+	while (x <= 'z')
+
+	{
+		putchar(x);
+		x++;
+	}
+
+	x = 'A';
+	while (x <= 'Z')
+	{
+		putchar(x);
+		x++;
+	}
+
+	putchar('\n');
+	return (0);
+}

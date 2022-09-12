5dce802bac7f8ab4647de2e127eeba7458c107e9
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-12 07:10:53 -0700

    variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,18 +3,13 @@
 #include <stdio.h>
 
 /**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ * Description: This is going give us if a random is positive or negative
+ * Return: 0 Value if it works
  *
- *  * main - Accept empty value
- *
- *   * write - Screen information
- *
- *    * @void: Description of a empty parameter
- *
- *      * Description: This is going give us if a random is positive or negative
- *
- *       * Return: 0 Value if it works
- *
- **/
+ */
 
 int main(void)
 

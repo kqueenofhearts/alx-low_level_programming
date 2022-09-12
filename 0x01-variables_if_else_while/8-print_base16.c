5f5b009db169c8bb57aedcae092dfcf36c8fb92a
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-12 09:16:21 -0700

    variable

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ * Description: This sort number from 9 to  16
+ * Return: 0 Value if it works
+ */
+
+int main(void)
+
+{
+
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+
+	{
+		putchar(x);
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+
+}
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+}

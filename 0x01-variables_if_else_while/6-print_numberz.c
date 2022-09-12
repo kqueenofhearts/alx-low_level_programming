a78b300fd8c170ae20b7f120bfaec5da0d84e7f2
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-12 08:50:06 -0700

    variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ * Description: This sort number from 9 to 10
+ * Return: 0 Value if it works
+ */
+
+int main(void)
+
+{
+
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}

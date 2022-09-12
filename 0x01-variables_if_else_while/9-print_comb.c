60dcbe6f05b8615c9ed08212a58d42f73ac5ab9f
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-12 09:31:23 -0700

    variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ * Description: This sort number from 9 to 10
+ * Return: 0 Value if it works
+ */
+
+int main(void)
+
+{
+
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if (!(x == '9' && y == '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

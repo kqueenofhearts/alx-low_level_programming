7ff3cd013001bc5a1bdd5480d5e431ad7de368f0
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-15 14:27:43 -0700

    loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,11 +11,11 @@
 void print_alphabet(void)
 
 {
-		char x;
+	char x;
 
-			for (x = 'a'; x <= 'z'; x++)
-					{
-						_putchar(x);
-					}
-			_putchar('\n');	
-}	
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');	
+}

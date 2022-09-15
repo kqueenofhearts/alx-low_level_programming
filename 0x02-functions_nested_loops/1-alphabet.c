fccbeede0f2e642f3f2158c014e7a5de5c1c8ffc
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-15 14:09:56 -0700

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet
+ * _putchar - Print characters one by one
+ *void: Description of a empty parameter
+ * Description: This prints the alphabet
+ * Return: 0 Value if it works
+ */
+
+void print_alphabet(void)
+
+{
+		char x;
+
+			for (x = 'a'; x <= 'z'; x++)
+					{
+				_putchar(x);
+					}
+				_putchar('\n');
+}				

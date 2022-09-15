917a35d706aabde92f86ce1f79e9f77d66181760
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-15 15:43:34 -0700

    loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Determine if a character is lowercase
- *@ c: The evaluated letter
+ *@c: The evaluated letter
  * Description: This prints 0 if the input is a lower and 1 if is not
  * Return: 0 Value if it works
  */

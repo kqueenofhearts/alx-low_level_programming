6b163e076588f7e9b47ef0a68c911c8230984742
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-16 17:59:09 -0700

    loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,16 @@
-#include "main.h"
+#include  "main.h"
 /**
  * prints_numbers - prints 01356789/n
  * Return void
  */
 void print_most_numbers(void);
-
 {
 	char i;
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (!(i == '2'; || i =='4'))
+		if (!(i == '2'; || i == '4'))
 			_putchar(i);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,8 +7,10 @@
 void print_line(int n);
 {
 	int i;
+
 	for (i = 0;  < n; i++)
 	{
 		_putchar('_');
-	}_putchar('\n');
+	}
+	_putchar('\n');
 }

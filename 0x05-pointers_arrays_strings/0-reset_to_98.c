19663907ee81c838979d47d7be72827e0b8c46cf
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-19 12:11:40 -0700

    Arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * reset_to_9-resets a pointerand updates its value
- * @n: the evaluated input
+ * @n: he evaluated input
  * Description: Takes a pointer to an int as parameter and updates value
  * Return empty
  */
 void reset_to_98(int *n);
-{
+ {
 	*n = 98;
-}
+ }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-et_to_98(int *n);
-void swap_int(int *a, int *
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);

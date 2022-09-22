4df2b636fe0fe884f697423cde7b8827446a2c9f
queenofhearts <kendisharon@stuudents.ac.ke>
2022-09-22 02:58:06 -0700

    loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,8 +3,8 @@
  * prints_numbers - prints 01356789/n
  * Return void
  */
-void print_most_numbers(void);
-{
+void print_most_numbers(void)
+	{
 	char i;
 
 	for (i = '0'; i <= '9'; i++)
@@ -13,4 +13,4 @@ void print_most_numbers(void);
 			_putchar(i);
 	}
 	_putchar('\n');
-}
+	}
